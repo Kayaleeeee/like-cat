f712e4bba586d9f4946ea73d1bca031a4ef0cfaa
Kaya Lee <60032720+Kayaleeeee@users.noreply.github.com>
2022-08-13 16:43:07 +0900

    install github pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from 'pages/Home';
 import LikedCatPage from 'pages/LikedCat';
 
@@ -8,6 +8,7 @@ const Router = () => {
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/liked-cats' element={<LikedCatPage />} />
+      <Route path='*' element={<Navigate replace to='/' />} />
     </Routes>
   );
 };
